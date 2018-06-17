6871e48491b04509323303f8f83041e299c7261d
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-06-17 13:38:42 +0530

    Create PLAYER11-TO PRINT IF IT IS A HOLIDAY.C

---
+++ PLAYER11-TO PRINT IF IT IS A HOLIDAY.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char a[100];
+    int i,l,cnt=0;
+    scanf("%s",a);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+   if(a[i]=='s'||a[i]=='a'||a[i]=='t'||a[i]=='u'||a[i]=='r'||a[i]=='d'||a[i]=='a'||a[i]=='y'||a[i]=='n')
+   {
+      cnt++;
+   }
+    }
+    if(cnt==8||cnt==6)
+    {
+        printf("holiday");
+    }
+   else{
+    printf("not holiday");
+   }
+}
